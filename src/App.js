ed52b39fd47392083500fd6517cff1bdca35ff6d
Adam-Young <awy6448@gmail.com>
2020-04-16 00:04:08 -0400

    refactored app to use a card file now and used styled components inside the card for the title and information inside. changed the navbar to now go to the offical page when clicking on the heading or the link provided on the navbar. MVP Complete.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
 import NavBar from "./components/navBar";
-import Img from "./components/img";
-import Information from './components/information';
+import styled from 'styled-components';
+import NasaCard from './components/card'
 
 
-
+const WraperDiv = styled.div`
+  width: 800px;
+  height: 100%;
+  display: flex;
+  box-sizing: border-box;
+  margin: auto;
+  flex-direction: column;
+`;
 
 
 
@@ -15,19 +22,23 @@ function App() {
   const [data, setData] = useState("")
   useEffect(() => {
     axios.get('https://api.nasa.gov/planetary/apod?api_key=eiDWTagkz26yWrq2Z9MZtYdYpVcdvutD4lvsoHoe')
-      .then(res => setData(res.data))
+      .then(res => {
+        setData(res.data)
+      })
       .catch(err => console.log(err))
   }, []);
 
   return (
-    <div className="App">
+    <WraperDiv className="App">
       <NavBar text={data.title} date={data.date}/>
 
-      <Img img={data.url} />
+      {/* <Img img={data.url} /> */}
 
-      <Information info={data.explanation}/>
+      {/* <Information info={data.explanation}/> */}
 
-    </div>
+      <NasaCard data={data}/>
+
+    </WraperDiv>
   );
 }
 
